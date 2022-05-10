1f6b43ed23cff88d65a143ae4383d3f967ca3545
solparrondo <101563449+solparrondo@users.noreply.github.com>
2022-05-10 15:31:59 -0300

    New scene added (la correcta)

---
+++ Assets/DataInput.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataInput : MonoBehaviour
+{
+    public Text txtUserName;
+
+    public void ShowGreeting()
+    {
+        Debug.Log("Hola" + txtUserName.text);
+    }
+}
